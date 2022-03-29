b13d4ce86a3508d0d025c60c9d7afa01e8087390
DESKTOP-FB5USM7\mgiov <mgiova96@gmail.com>
2022-03-29 20:46:05 +0200

    First commit

---
+++ quiz-bot/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ContextProvider } from './hooks/useStateContext';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline>
+          <App />
+        </CssBaseline>
+      </ThemeProvider>
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
